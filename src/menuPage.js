332564a79e5059addf76967f286a69b795c36f3f
grubd14 <83056016+grubd14@users.noreply.github.com>
2023-08-01 19:12:10 +0545

    feat: basic menu page

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,5 +1,21 @@
+export { createMenuSection };
 
 function createMenuSection() {
-  const contentBody = document.getElementById('content');
+  const contentBody = document.getElementById("content");
 
+  const menuItemsContainer = document.createElement("div");
+  menuItemsContainer.classList.add("menu-container");
+
+  const menuSection = document.createElement("div");
+  menuSection.classList.add("menu-items");
+  menuSection.innerHTML = `
+<h4>Menu Items</h4>
+<ul>Chicken Tandoori</ul>
+<ul>Chicken Tandoori</ul>
+<ul>Chicken Tandoori</ul>
+<ul>Chicken Tandoori</ul>
+<ul>Chicken Tandoori</ul>`;
+
+  contentBody.appendChild(menuItemsContainer);
+  menuItemsContainer.appendChild(menuSection);
 }
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,8 +1,10 @@
 import { createAboutSection } from "./homePage";
 import { createTopNavigationBar } from "./navigationBar";
+import { createMenuSection } from "./menuPage";
 export { pageLoad };
 
 function pageLoad() {
   createTopNavigationBar();
   createAboutSection();
+  createMenuSection();
 }

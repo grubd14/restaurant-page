f330002b9fb32dd82df222d5ba7b6d611c28977d
grubd14 <83056016+grubd14@users.noreply.github.com>
2023-08-01 18:54:23 +0545

    fix: typo in webpack config preventing compiling of assest feat: finish createAboutSection() which renders Home page with Image and Text fix: change Image asset to better image fix: apply some basic padding and margin rules

--- src/homePage.js
+++ src/homePage.js
@@ -1,13 +1,25 @@
 export { createAboutSection };
+import RestaurantImage from "./restaurant.jpg";
 
 function createAboutSection() {
-  const contentBody = document.getElementById('content')
+  const contentBody = document.getElementById("content");
   const aboutSectionContainer = document.createElement("div");
 
   aboutSectionContainer.classList.add("about-section");
 
+  const aboutSectionImage = document.createElement("div");
+  aboutSectionImage.classList.add("about-image-container");
+
+  const restroImage = new Image();
+  restroImage.src = RestaurantImage;
+  restroImage.classList.add("about-section-image");
+
+  const aboutSectionText = document.createElement("div");
+  aboutSectionText.classList.add("about-section-text");
+  aboutSectionText.innerHTML = `<p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat repur Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>`;
+
+  aboutSectionContainer.appendChild(aboutSectionImage);
+  aboutSectionContainer.appendChild(aboutSectionText);
+  aboutSectionImage.appendChild(restroImage);
   contentBody.appendChild(aboutSectionContainer);
-
-
 }
-
---
+++ src/menuPage.js
@@ -0,0 +1,5 @@
+
+function createMenuSection() {
+  const contentBody = document.getElementById('content');
+
+}
